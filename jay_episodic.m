e81c64b49a57a5e08761d79bcc4519a5ec9b1fd6
Emily Rounds <elrounds@gmail.com>
2014-07-10 10:22:10 -0700

    May have values that work for all conditions now. Do a long run.

--- jay_episodic.m
+++ jay_episodic.m
@@ -28,7 +28,7 @@ INP_STR = 5;
 gain_step = .04;
 gain_max = 0.7;
 
-runs = 20;
+runs = 5;
 cycles = 9;
 % cycles = 8;
 
@@ -37,14 +37,14 @@ global PILF;
 global DEGR;
 
 %      Worm   Peanut
-REPL = [ 5.0   2.0];
-PILF = [ 0.0   2.0];
-DEGR = [-6.0   2.0]; % O X
+REPL = [ 3.0   1.0];
+PILF = [ 0.0   1.0];
+DEGR = [-4 .0   1.0]; % O X
 %hpc: peanut crazy if training ends on degrade, perfect if it ends on worm.
 %pfc: prefers flip of what was last presented...
 
 gain_oja = 0.7;
-learning_rate = 0.45;
+learning_rate = 0.4;
 pfc_learning_rate = 0.01;
 
 
